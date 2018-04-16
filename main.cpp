f9df7f3c418d3c2d9377447c5d6ecd566b95bee1
baku1413 <33036538+baku1413@users.noreply.github.com>
2018-04-16 19:31:47 +0200

    Pierwsza wersja programu
    ##Zaimplementowano 
    -sprawdzenie, czy plik został przeciągnięty.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <conio.h>
+
+using namespace std;
+
+int main(int arg, char *argumenty[])
+
+{
+
+    if (arg<2)
+    {
+            cout<<"¿aden plik nie zosta³ przeci¹gniêty"<<endl;
+            getch();
+
+    }
+    else
+    {
+    cout<<"odczytano plik"<<endl;
+    getch();
+    }
+
+}
