394a461dfe2302910bfeb2302d788b31a0d18bb4
baku1413 <33036538+baku1413@users.noreply.github.com>
2018-05-20 14:00:27 +0200

    Odczyt kolumn
    -Dodano odczytywanie poszczególnej kolumny z pliku tekstowego
    -Usunięto (na razie) odczyt przeciąganego pliku

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
-#include <fstream>
 #include <sstream>
+#include <fstream>
 #include <cstdio>
 #include <conio.h>
-
+#include <list>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
-
+/*class Student
+{
+    private std::string imie;
+    //....
+    //std::list<float>
+};*/
 int main(int arg, char *argumenty[])
 
 {
-
-    if (arg<2)
+    if(0)
+    //if (arg<2)
     {
             cout<<"¿aden plik nie zosta³ przeci¹gniêty"<<endl;
             getch();
@@ -18,23 +26,42 @@ int main(int arg, char *argumenty[])
     }
     else
     {
+        ifstream myfile ("C:\\Users\\Teresa\\Desktop\\IT project\\ProjectIT\\Nowy dokument tekstowy.txt");
+        //
         cout<<"odczytano plik"<<endl;
-        fstream plik;
-        plik.open(argumenty[1]);
+        string b = "1001";
+        int wynik = atoi( b.c_str() );
+        cout<<wynik+100<<endl;
+        //
         int a=0;
         int odcztint =0;
         int suma=0;
         float srednia=0;
-        while(plik>>odcztint)
+        std::string line;
+        if (myfile.is_open())
+        {
+            while (std::getline(myfile, line))
             {
-                suma=suma+odcztint;
-                a++;
+                if(line!="")
+                {
+                    vector<string> s;
+                    std::istringstream iss(line);
+                    for( std::string line; iss >> line; )
+                    {
+                        s.push_back(line);
+                    }
+                    //new Student(result[0])
+                    std::cout<<s[1]<<endl;
+                }
             }
-            srednia=suma/a;
-        cout<<"program ma: "<<a<<"linijek"<<endl;
-        cout<<"suma liczb to: "<<suma<<endl;
-        cout<<"srednia z liczb to: "<<srednia<<endl;
+        }
+        //cout<<"program ma: "<<a<<"linijek"<<endl;
+        //cout<<"suma liczb to: "<<suma<<endl;
+        //cout<<"srednia z liczb to: "<<srednia<<endl;
+        //getch();
+        myfile.close();
         getch();
+        return 1;
     }
 
 }
