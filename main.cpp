89fa259ae6b9cda059d7d30c2865626667717d0b
baku1413 <33036538+baku1413@users.noreply.github.com>
2018-06-17 15:16:28 +0200

    Dodanie flagi ze statusem zaliczenia
    Uporządkowano kod
    Zrobiono z programu telegazetę
    dodano formatowanie i odpowiednie wyrównywanie kolumn
    zaimplementowano obsługę dowolnej ilości wyrazów w kolumnach

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+
 using namespace std;
 class Student
 {
@@ -15,24 +16,22 @@ class Student
     string nazwisko;
     string przedmiot;
     unsigned int srednia;
-    //super u¿yteczna klasa polecam
+    //super uÂ¿yteczna klasa polecam
 };
 
-class MurzynLiczacySrednia
+class Liczeniesredniej
 {
 public:
 
 string oceny;
 float srednia;
 
-
 float SrZeStringa(string x)
 
 {
-    int k=0;
     float srednia=0;
     int j=0;
-    int n = 0;
+    float n = 0;
     char c='a';
     string mySt = x;
     int L = mySt.length()-1;
@@ -46,46 +45,13 @@ float SrZeStringa(string x)
         j++;
     }
     }
-
     srednia = n/j;
     return srednia;
 }
-
 
 };
 
-/*
-float SrZeStringa(string x)
-
-{
-    int k=0;
-    float srednia=0;
-    int j=0;
-    int n = 0;
-    char c='a';
-    string mySt = x;
-    int L = mySt.length()-1;
-    for(int i=0;i<=L;i++)
-    {
-    c = mySt.at(i);
-
-    if(isdigit(c))
-    {
-        n+=(c - '0');
-        j++;
-    }
-    }
-
-    srednia = n/j;
-    return srednia;
-}
-
-*/
-
-
 int main(int arg, char *argumenty[])
-
-
 {
 
 
@@ -95,56 +61,83 @@ int main(int arg, char *argumenty[])
     {
             cout<<"zaden plik nie zostal przeciagniety"<<endl;
             getch();
-
     }
     else
     {
-        ifstream myfile ("C:\\Users\\Bartek\\Desktop\\IT project\\ProjectIT\\Nowy dokument tekstowy.txt");
-        //ifstream myfile (argumenty[1]);
-        cout<<"IMIE|NAZWISKO|PRZEDM|OCENY       |SREDNIA"<<endl;
-        /*
-        string b = "1001";
-        int wynik = atoi( b.c_str() );
-        cout<<wynik+100<<endl;
-        */
+            int x=0;
+            float flaga =0;
+            string oceny;
+            string line;
+            ifstream myfile ("C:\\Users\\Bartek\\Desktop\\lmeo\\Projekt-IT-sem.-II-master\\Nowy dokument tekstowy.txt");
+            //ifstream myfile (argumenty[1]);
+            system("Color 7D");
+            cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+            cout<<right<<"                    Podaj srednia od ktorej jest zaliczenie"<<endl<<endl;
+            cout<<right<<"                                      ";
+            cin>>flaga;
+            system("CLS");
+            system("Color 3A");
+            cout.width( 10 );
+            cout<<"|IMIE|";
+            cout.width( 13 );
+            cout<<"|NAZWISKO|";
+            cout.width( 13 );
+            cout<<"|PRZEDM|";
+            cout.width( 17 );
+            cout<<"|OCENY|";
+            cout.width( 20 );
+            cout<<"|SREDNIA|"<<endl;
+            cout<<endl;
 
-        MurzynLiczacySrednia BlackNiBBa;
+            Liczeniesredniej srednia;
 
-        string oceny;
-        int kol=3;
-        int i=0;
-        int odcztint =0;
-        std::string line;
         if (myfile.is_open())
         {
-            while (std::getline(myfile, line))
+            while (getline(myfile, line))
             {
                 if(line!="")
                 {
                     vector<string> s;
-                    std::istringstream iss(line);
-                    for( std::string line; iss >> line; )
+                    istringstream iss(line);
+                    for(string line; iss >> line;)
                     {
+
                         s.push_back(line);
+                        x++;
                     }
-                    //new Student(result[0])
-                    std::cout<<s[0]<<" ";
-                    std::cout<<s[1]<<" ";
-                    std::cout<<s[2]<<" ";
-                    std::cout<<s[3]<<" ";
-                    //if (kol==3)
-                    //{
+                            x=x-1;
+                            cout.precision(3);
+                            cout.width( 10 );
+                            cout<<left<<s[0]<<" ";
+                            cout.width( 13 );
+                            cout<<s[1]<<" ";
+                            cout.width( 13 );
+                            cout<<s[2]<<" ";
+                            cout.width( 17 );
 
-                    cout<<"      Srednia:"<<BlackNiBBa.SrZeStringa(s[3])<<endl;
+                            cout<<s[x]<<" ";
+                            cout.width( 3 );
+                            cout.setf( ios::showpoint );
+                            cout<<"Sr:"<<srednia.SrZeStringa(s[x]);
 
-                    //}
+                                if (srednia.SrZeStringa(s[x]) < flaga)
+                                    {
+
+                                        cout.width( 12 );
+                                        cout<<right<<" Niezaliczone"<<endl;
+                                        x=0;
+                                    }
+                                else
+
+                                    {
+                                        cout.width( 12 );
+                                        cout<<right<<" Zaliczone"<<endl;
+                                        x=0;
+                                    }
                 }
             }
         }
-        //cout<<"program ma: "<<a<<"linijek"<<endl;
-        //cout<<"suma liczb to: "<<suma<<endl;
-        //cout<<"srednia z liczb to: "<<srednia<<endl;
-        //getch();
+
         myfile.close();
         getch();
         return 1;
